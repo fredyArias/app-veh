ec6f86f3ad93d7f4d737862c6e73251c4782cbe6
Fredy Arias <duvanar@MacBook-Air-de-Fredy.local>
2022-06-01 17:57:35 -0500

    chore: delete comment

--- src/Controller/SearchController.ts
+++ src/Controller/SearchController.ts
@@ -2,7 +2,7 @@ import { DB } from "../Model/DB";
 import { SearchView } from "../View/SearchView";
 
 export class SearchController {
-    searchView: SearchView // es valído instanciar objetos está forna (sin el new)?
+    searchView: SearchView 
     valueToSearch: string
 
     constructor() {
